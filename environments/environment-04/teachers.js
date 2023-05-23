2a0b4c1d5feb62b4c69e7e8d2ad8fe9671d45a49
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-05-23 09:15:00 +0200

    environment 4 added

---
+++ environments/environment-04/teachers.js
@@ -0,0 +1,18 @@
+const teachers = [
+    {
+        name: "Jane Smith",
+        email: "jane.smith@email.com"
+    },
+    {
+        name: "Bob Johnson",
+        email: "bob.johnson@email.com"
+    },
+    {
+        name: "Mark Lee",
+        email: "mark.lee@email.com"
+    },
+    {
+        name: "Julia Kim",
+        email: "julia.kim@email.com"
+    }
+];
