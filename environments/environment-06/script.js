ab80fd0d39f40d6b764879bef0e3bea9e5bb8e4d
Peter Lind (@KEA) <petl@kea.dk>
2023-05-25 13:53:57 +0200

    solution - don't push

--- environments/environment-06/script.js
+++ environments/environment-06/script.js
@@ -36,27 +36,85 @@ function showProduct(product) {
   document.querySelector("#products article:last-child button").addEventListener("click", clickPutInBasket);
 
   function clickPutInBasket(event) {
-    putInBasket(product);
+    addToBasket(product);
+    showBasket();
   }
 }
 
 // læg i kurv
 let basket = [];
-function putInBasket(product) {
-  basket.push(product);
-
-  showInBasket(product);
+function addToBasket(product) {
+  // check if product is in basket
+  let obj = basket.find(obj => obj.product === product);
+  if(!obj) {
+    obj = {product: product, antal: 0};
+    basket.push(obj);
+  }
+  
+  obj.antal++;  
 }
 
-function showInBasket(product) {
+// fjern fra kurv
+function removeFromBasket(product) {
+  let obj = basket.find(obj => obj.product === product);
+  if(obj) {
+    obj.antal--;
+    if(obj.antal <= 0) {
+      const index = basket.indexOf(obj);
+      basket.splice(index,1);
+    }
+  }
+}
+
+function showBasket() {
+  document.querySelector("#basket tbody").innerHTML = "";
+  basket.forEach(showInBasket)
+  showBasketTotals();
+}
+
+function showInBasket(obj) {
   const HTML = /*html*/`
   <tr>
-    <td><input type="number" value="1" /></td>
-    <td>${product.name}</td>
-    <td>${product.price},-</td>
-    <td>${product.price},-</td>
+    <td>
+      <button class="remove">-</button>
+      ${obj.antal}
+      <button class="add">+</button>
+    </td>
+    <td>${obj.product.name}</td>
+    <td>${obj.product.price},-</td>
+    <td>${obj.product.price * obj.antal},-</td>
   </tr>
   `;
 
   document.querySelector("#basket tbody").insertAdjacentHTML("beforeend", HTML);
+
+  document.querySelector("#basket tbody tr:last-child button.remove").addEventListener("click", () => {removeFromBasket(obj.product); showBasket();});
+  document.querySelector("#basket tbody tr:last-child button.add").addEventListener("click", () => {addToBasket(obj.product); showBasket();});
+
+
+}
+
+function showBasketTotals() {
+  const productsInBasket = basket.length;
+  let totalProducts = 0;
+  let totalPrice = 0;
+  let totalWeight = 0;
+  for(const obj of basket) {
+    totalProducts += obj.antal;
+    totalPrice += obj.product.price * obj.antal;
+    totalWeight += obj.product.weight * obj.antal;
+  }
+
+  document.querySelector("#total-in-basket").textContent = productsInBasket;
+  document.querySelector("#total-products").textContent = totalProducts;
+  document.querySelector("#total-price").textContent = totalPrice;
+  document.querySelector("#total-weight").textContent = totalWeight;
+
+  if(totalWeight>=2000) {
+    document.querySelector(".warning").classList.add("show");
+  } else {
+    document.querySelector(".warning").classList.remove("show");
+  }
+
+
 }
