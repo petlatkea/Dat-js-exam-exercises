3d111b6ca6d55971d53b63b2bfba56fe0d75cbef
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-05-31 11:58:17 +0200

    Update script.js

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,36 @@
 "use strict";
+
+let songs = [];
+
+window.addEventListener("load", initApp);
+
+async function initApp() {
+    songs = await getSongs();
+    console.log(songs);
+    showSongs();
+}
+
+async function getSongs() {
+    const response = await fetch("playlist.json");
+    const data = await response.json();
+    return data;
+}
+
+function showSongs() {
+    document.querySelector("#songlist").innerHTML = "";
+    for (const song of songs) {
+        const html = /*html*/ `
+            <li>${song.artist} (${song.duration}), ${song.title} <button>Remove</button></li>
+        `;
+        document.querySelector("#songlist").insertAdjacentHTML("beforeend", html);
+        document
+            .querySelector("#songlist li:last-child button")
+            .addEventListener("click", () => removeSong(song));
+    }
+}
+
+function removeSong(songToRemove) {
+    console.log(songToRemove);
+    songs = songs.filter(song => song.title != songToRemove.title);
+    showSongs();
+}
