72397b34f9d55069a1553974c10dc2a4bbe05793
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-05-31 12:05:33 +0200

    Update script.js

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,23 +1,32 @@
 "use strict";
 
-const songs = [
-    { artist: "Taylor Swift", title: "Blank space", duration: "4:33" },
-    { artist: "Beastie Boys", title: "Sabotage", duration: "3:02" },
-    { artist: "Skrillex", title: "Bangarang", duration: "3:35" },
-    { artist: "Wolfgang Amadeus Mozart", title: "Eine kleine Nachtmusik", duration: "5:45" },
-    { artist: "Coldplay", title: "Yellow", duration: "4:27" },
-    { artist: "Metallica", title: "Enter Sandman", duration: "5:32" }
-];
+const songs = [];
 
 window.addEventListener("load", initApp);
 
 function initApp() {
     console.log(songs);
-    // showSongs();
-    sortBy("artist");
+    showSongs();
 
+    document.querySelector("#add-song-form").addEventListener("submit", createFormSubmit);
     document.querySelector("#sort-artist").addEventListener("change", sortChanged);
     document.querySelector("#sort-title").addEventListener("change", sortChanged);
+}
+
+function createFormSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const artist = form.name.value;
+    const title = form.title.value;
+    const duration = form.duration.value;
+
+    createSong(artist, title, duration);
+    showSongs();
+}
+
+function createSong(artist, title, duration) {
+    const song = { artist, title, duration };
+    songs.push(song);
 }
 
 function showSongs() {
