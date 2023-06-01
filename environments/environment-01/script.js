619a25b04251943a8385938e4579afe1870eac65
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-06-01 12:35:39 +0200

    Update script.js

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,33 @@
 "use strict";
 
+window.addEventListener("load", initApp);
+
+let usersGlobal = [];
+
+async function initApp() {
+    usersGlobal = await getUsers();
+    displayUsers(usersGlobal);
+
+    // displayAdmins(users);
+}
+
+async function getUsers() {
+    const response = await fetch("users.json");
+    const data = await response.json();
+    return data;
+}
+
+function displayUsers(listOfUsers) {
+    for (const user of listOfUsers) {
+        const html = /*html*/ `
+            <li>${user.name} (${user.role}), active: ${user.active}</li>
+        `;
+        console.log(html);
+        document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+    }
+}
+
+function displayAdmins() {
+    const admins = usersGlobal.filter(user => user.role === "admin");
+    displayUsers(admins);
+}
